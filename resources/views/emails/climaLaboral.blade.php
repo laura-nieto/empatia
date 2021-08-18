c79c4768e639783f842a378c02c2d0fcb56d250f
Maria Laura Nieto <nm.laura01@gmail.com>
2021-08-18 14:40:04 -0300

    Valign center mails

--- resources/views/emails/climaLaboral.blade.php
+++ resources/views/emails/climaLaboral.blade.php
@@ -286,7 +286,7 @@ a {text-decoration: none;}
         <table class="es-wrapper" width="100%" cellspacing="0" cellpadding="0"
             style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;padding:0;Margin:0;width:100%;height:100%;background-repeat:repeat;background-position:center top">
             <tr>
-                <td valign="top" style="padding:0;Margin:0">
+                <td valign="center" style="padding:0;Margin:0">
                     <table cellpadding="0" cellspacing="0" class="es-content" align="center"
                         style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;table-layout:fixed !important;width:100%">
                         <tr>
