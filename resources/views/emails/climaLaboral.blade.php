68f372de882940368ff83b7e4a13b4babb6e4f87
Maria Laura Nieto <nm.laura01@gmail.com>
2021-08-20 16:13:37 -0300

    email borrado td

--- resources/views/emails/climaLaboral.blade.php
+++ resources/views/emails/climaLaboral.blade.php
@@ -298,7 +298,7 @@ a {text-decoration: none;}
                                         <td align="left"
                                             style="padding:0;Margin:0;padding-top:20px;padding-left:20px;padding-right:20px">
                                             <!--[if mso]><table style="width:520px" cellpadding="0"
-cellspacing="0"><tr><td style="width:160px" valign="middle"><![endif]-->
+                                                cellspacing="0"><tr><td style="width:160px" valign="middle"><![endif]-->
                                             <table cellpadding="0" cellspacing="0" class="es-left" align="left"
                                                 style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;float:left">
                                                 <tr>
@@ -329,7 +329,7 @@ cellspacing="0"><tr><td style="width:160px" valign="middle"><![endif]-->
                                         </td>
                                         <td valign="middle" height="100%" align="right"
                                         style="padding:0;Margin:0;padding-top:20px;padding-left:20px;padding-right:20px;width:340px">
-                                            <!--[if mso]></td><td style="width:20px"></td><td style="width:340px;display:flex;align-items:center;" height="100" valign="middle" align="right"><![endif]-->
+                                            <!--[if mso]></td><td style="width:340px;display:flex;align-items:center;" height="100" valign="middle" align="right"><![endif]-->
                                             {{-- <table cellpadding="0" cellspacing="0" align="right" height="100%"
                                                 style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px">
                                                 <tr>
