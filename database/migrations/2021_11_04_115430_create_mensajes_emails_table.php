1256227978fd973d03d53d73a0d239188bba1664
Maria Laura Nieto <nm.laura01@gmail.com>
2021-11-05 12:10:26 -0300

    modificacion para migrar en heroku

--- database/migrations/2021_11_04_115430_create_mensajes_emails_table.php
+++ database/migrations/2021_11_04_115430_create_mensajes_emails_table.php
@@ -13,6 +13,9 @@ class CreateMensajesEmailsTable extends Migration
      */
     public function up()
     {
+        Schema::table('datos_demograficos', function (Blueprint $table) {
+            $table->foreignId('user_id')->nullable()->constrained('users')->onDelete('cascade');
+        });
         Schema::create('mensajes_emails', function (Blueprint $table) {
             $table->id();
             $table->mediumText('clima')->nullable();
